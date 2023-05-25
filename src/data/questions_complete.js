2f17c9eef51d89961a1719b3c8fbe2fc95802012
FabioCostaa <fabio.glz4@gmail.com>
2023-05-25 15:50:41 -0300

    first commit

---
+++ src/data/questions_complete.js
@@ -0,0 +1,83 @@
+const data = [  
+  {
+    category: "Marvel",
+    questions: [
+      {
+        question: "Qual super-herói da Marvel tem o poder de se regenerar rapidamente, além de garras retráteis ?",
+        options: ["Homem de Ferro", "Doutor Estranho", "Wolverine", "Thor"],
+        answer: "Wolverine",
+        tip: "Seu nome é Logan",
+      },
+      {
+        question: "Quem é conhecido como o Deus do Trovão ?",
+        options: ["Homem-Aranha", "Doutor Estranho", "Capitão América", "Thor"],
+        answer: "Thor",
+        tip: "Possui um martelo com o nome de 'Mjolnir' ",
+      },
+      {
+        question: "Qual é o nome verdadeiro do Homem de Ferro ?",
+        options: ["Tony Stark", "Bruce Banner", "Peter Parker", "Steve Rogers"],
+        answer: "Tony Stark",
+        tip: "Ele é CEO de uma empresa que tem o seu sobrenome como nome",
+      },
+      {
+        question: "Quem é Steve Rogers ?",
+        options: [
+          "Homem de Ferro",
+          "Capitão América",
+          "Homem-Aranha.",
+          "Thor"],
+        answer: "Capitão América",
+        tip: "Ele usa um escudo redondo como sua principal arma",
+      },
+      {
+        question: "Qual é o nome do líder dos Vingadores ?",
+        options: ["Homem de Ferro", "Thor", "sHulk", "Capitão América"],
+        answer: "Capitão América",
+        tip: "Passou anos congelado.",
+      },
+    ],
+  },
+  {
+    category: "DC",
+    questions: [
+      {
+        question: "Quem é conhecido como o Homem de Aço ?",
+        options: [
+          "Batman",
+          "Flash",
+          "Cyborg",
+          "Superman",
+        ],
+        answer: "Superman",
+        tip:"Seu nome é Clark Kent",
+      },
+      {
+        question: "Qual é o nome do planeta natal do Superman?",
+        options: ["Apokolips", "Themyscira", "Asgard", "Krypton"],
+        answer: "Krypton",
+        tip:"O planeta foi destruído"
+      },
+      {
+        question: "Qual é o nome da ilha habitada exclusivamente por amazonas ?",
+        options: ["Themyscira", "Atlantis", "Apokolips", "Arkham Asylum"],
+        answer: "Themyscira",
+        tip: "Lugar onde a Mulher-Maravilha foi criada",
+      },
+      {
+        question: "Quem é o arqui-inimigo do Lanterna Verde ?",
+        options: ["Ra's al Ghul", "Deadshot", "Darkseid", "Sinestro"],
+        answer: "Sinestro",
+        tip:"Ele é vermelho",
+      },
+      {
+        question:"Qual é o nome do vilão principal em Aquaman ?",
+        options: ["Arraia Negra", "Ocean Master", "Crocodilo", "Manta Negra"],
+        answer: "Ocean Master",
+        tip:"Meio-irmão do Aquaman",
+      },
+    ],
+  },
+];
+
+export default data;
